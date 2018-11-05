9807c00d8427bfecb7dbda961768b0393ec64674
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-11-05 17:34:36 -0600

    Final examples using macros.

--- src/7. Macros/example.clj
+++ src/7. Macros/example.clj
@@ -32,3 +32,32 @@
   `(let [value# ~expr]
      (println "Debug" ~(list 'quote expr) ":" value#)
      value#))
+
+(defmacro AND
+  ([] true)
+  ([x] x)
+  ([a b & exprs]
+   `(let [temp# ~a]
+      (if temp#
+        (AND ~b ~@exprs)
+        temp#))))
+
+(defn find-between
+  "Returns a sequence of elements in lst
+  contained exclusively between start and end."
+  [start end lst]
+  (->> lst
+       (drop-while #(not= start %))
+       rest
+       (take-while #(not= end %))))
+
+(defmacro IF
+  "Provides a conditional statement that is syntactically
+   a bit more similar to those found in languages like
+   Pascal or Fortran. It has the following form:
+
+   (IF condition :THEN exp1 exp2 ... :ELSE exp3 exp4 ...)"
+  [condition & exprs]
+  `(if ~condition
+     (do ~@(find-between :THEN :ELSE exprs))
+     (do ~@(find-between :ELSE :THEN exprs))))
